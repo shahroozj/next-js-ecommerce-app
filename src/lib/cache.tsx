6482e6cd90486751b4e44a405abacc05c029b65d
shahroozj <scorpion_s@yahoo.com>
2025-01-06 19:20:56 +0800

    add comment

--- src/lib/cache.tsx
+++ src/lib/cache.tsx
@@ -1,6 +1,7 @@
 import { unstable_cache as nextCache} from "next/cache";
 import { cache as reactCache} from "react";
 
+//because we want to use both nextCaching and reactCache, we need to create a cache object extending the both.
 type Callback = (...args: any[]) => Promise<any>;
 export function cache<T extends Callback>(cb: T, keyParts: string[], options:{ revalidate?: number | false; tags?: string[]} = {}){
     return nextCache(reactCache(cb), keyParts, options);
